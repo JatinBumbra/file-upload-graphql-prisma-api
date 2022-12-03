a3cbdc706b551def865b990184eaa4c441c41ce5
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-12-03 17:43:46 +0530

    CRUD files

--- file/schema.ts
+++ file/schema.ts
@@ -1,7 +1,7 @@
+import { File } from "@prisma/client"
 import { prismaClient } from "../prisma"
 import { createModule, gql } from "graphql-modules"
 import * as fileService from "./service"
-import { File } from "@prisma/client"
 
 export const fileModule = createModule({
   id: "file-module",
@@ -31,26 +31,42 @@ export const fileModule = createModule({
 
       extend type Query {
         getAllFiles: [File]!
+        getFile(id: ID!): File
       }
 
       extend type Mutation {
         createFile(input: CreateFileInput!): CreateFileResult!
+        moveFile(id: ID!, directoryId: ID!): File!
+        renameFile(id: ID!, name: String!): File!
+        deleteFile(id: ID!): Boolean!
       }
     `,
   ],
   resolvers: {
     Query: {
-      getAllFiles: () => {
-        return prismaClient().file.findMany()
-      },
+      getAllFiles: () => prismaClient().file.findMany(),
+      getFile: async (_: unknown, { id }: { id: File["id"] }) =>
+        await fileService.getFile(prismaClient(), id),
     },
     Mutation: {
       createFile: async (
         _: unknown,
         { input }: { input: fileService.CreateFileInput }
-      ): Promise<{ file: File; url: string }> => {
-        return await fileService.createFileRecord(prismaClient(), input)
-      },
+      ): Promise<{ file: File; url: string }> =>
+        await fileService.createFileRecord(prismaClient(), input),
+      moveFile: async (
+        _: unknown,
+        {
+          id,
+          directoryId,
+        }: { id: File["id"]; directoryId: File["directoryId"] }
+      ) => await fileService.moveFile(prismaClient(), id, directoryId),
+      renameFile: async (
+        _: unknown,
+        { id, name }: { id: File["id"]; name: File["name"] }
+      ) => await fileService.renameFile(prismaClient(), id, name),
+      deleteFile: async (_: unknown, { id }: { id: File["id"] }) =>
+        await fileService.deleteFile(prismaClient(), id),
     },
   },
 })
